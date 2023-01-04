8ab20ecbecffb82d57e069424e70980b2033f950
Andi Muhaxheri <amuhaxheri@wonderworks.gg>
2023-01-04 09:33:44 -0600

    project name fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'kinqandi', // Usually your GitHub org/user name.
-  projectName: 'dumpster-api', // Usually your repo name.
+  projectName: 'Dumpster-API', // Usually your repo name.
 deploymentBranch: "gh-pages",
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
