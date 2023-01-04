4fecaebf94dfad9d36139aa59f7f0548a79f9024
Andi Muhaxheri <amuhaxheri@wonderworks.gg>
2023-01-04 09:23:16 -0600

    api fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,16 +9,16 @@ const config = {
   title: 'Dumpster',
   tagline: 'API',
   url: 'https://github.com',
-  baseUrl: 'https://github.com',
+  baseUrl: '/dumpster-api/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'kinqandi', // Usually your GitHub org/user name.
+  projectName: 'dumpster-api', // Usually your repo name.
+deploymentBranch: "gh-pages",
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
